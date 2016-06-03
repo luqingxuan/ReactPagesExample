7b37ff97918ca6dd864bb4e1e59da40ab8bf7f1f
qingjie.cheng <qingjie.cheng@datayes.com>
2016-06-03 12:18:21 +0800

    Change-Id: I37a4ce0320b450850f3da557f2e0006e335d0662

--- src/js/common/plugins.js
+++ src/js/common/plugins.js
@@ -4,7 +4,7 @@ require('../../css/lib/hover.css');
 
 require('font-awesome-webpack!../../../font-awesome.config.js');
 
-//本地存储插件
+// 本地存储插件
 require('jstorage');
 
 // cookie
@@ -12,8 +12,8 @@ require("js-cookie");
 
 // 遮罩插件
 // require('imports?jQuery=jquery,$=jquery,this=>window!../lib/jquery/loadmask/jquery.loadmask.js');
-require('../lib/jquery/loadmask/jquery.loadmask.js');
-require('../lib/jquery/loadmask/jquery.loadmask.css');
+// require('../lib/jquery/loadmask/jquery.loadmask.js');
+require("jquery.loadmask");
 
 // 弹出提示插件
 require('../lib/jquery/confirm/jquery.confirm.js');
